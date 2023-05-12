c47d9efd80ae4adf5a2e3daa46648fdbac2313a5
saloni0824 <133234014+saloni0824@users.noreply.github.com>
2023-05-12 22:29:28 +0530

    Create Task1.cpp

---
+++ Task1.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class person {
+private:
+    string name;
+    string DOB;
+    string email;
+
+public:
+void entry(){
+    cout<< "Enter name: ";
+    cin>> this->name;
+    cout << "Enter DOB: ";
+    cin >> this->DOB;
+    cout << "Enter Email: ";
+    cin >> this->email;
+}
+
+void print(){
+    cout << "Name   : "<< this->name << endl;
+    cout << "DOB    : "<< this->DOB << endl;
+    cout << "Email  : "<< this->email << endl;
+
+}
+
+
+};
+
+int main() {
+    vector<person> member;
+    int n;
+    cout<<"Enter No. of Members: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        member.push_back(person());
+    }
+    for(int i=0;i<n;i++){
+        member[i].entry();
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        member[i].print();
+        cout<<endl;
+    }
+
+    
+
+    /*int num;
+    cout<<"No. of Members: ";
+    cin>>num;
+
+    
+    for(int i=0;i<num;i++){
+        char j=i;
+        person test+j;
+        test.entry();
+    }*/
+
+    return 0;
+}
